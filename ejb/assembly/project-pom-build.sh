a2649959f703f49def0a1219b5b8035b7489bd10
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2024-01-08 11:36:36 -0500

    Improve build.sh

--- ejb/assembly/project-pom-build.sh
+++ ejb/assembly/project-pom-build.sh
@@ -60,7 +60,7 @@ menu_actions()
 			"e)")	${BUILDER} dependency:tree ;;
 			"f)")	${BUILDER} dependency:resolve-plugins ;;
 			"g)")	${BUILDER} dependency:analyze ;;
-			"h)")	${BUILDER} dependency:sources dependency:resolve -Dclassifier=javadoc ;;
+			"h)")	${BUILDER} dependency:sources; mvn dependency:resolve -Dclassifier=javadoc ;;
 			"i)")	${BUILDER} -DskipTests=false clean test ;;
 			"j)")	${BUILDER} -DskipTests=true  clean package ;;
 			"k)")	${BUILDER} -DskipTests=true  clean install ;;
