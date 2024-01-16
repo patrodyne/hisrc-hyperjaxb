3e1c0968527355cd86c2b3d8541287482fcf0fda
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2024-01-16 17:08:21 -0500

    Bump HiSrc POM to version 2.2.0-SNAPSHOT

--- sql-cli-h2db.sh
+++ sql-cli-h2db.sh
@@ -34,8 +34,8 @@ if ! command -v rlwrap >/dev/null; then
 	echo "	Home: https://github.com/hanslub42/rlwrap"
 	echo "	Debian: sudo apt update; sudo apt install rlwrap"
 fi
-H2JAR="${M2_REPO}/com/h2database/h2/2.2.220/h2-2.2.220.jar"
-PGJAR="${M2_REPO}/org/postgresql/postgresql/42.6.0/postgresql-42.6.0.jar"
+H2JAR="${M2_REPO}/com/h2database/h2/2.2.224/h2-2.2.224.jar"
+PGJAR="${M2_REPO}/org/postgresql/postgresql/42.7.1/postgresql-42.7.1.jar"
 if [ ! -r "${H2JAR}" ]; then
 	echo "ERROR: Please configure H2JAR location in this script."
 	exit 1
--- sql-web-h2db.sh
+++ sql-web-h2db.sh
@@ -33,8 +33,8 @@
 # 
 # See also https://h2database.com/javadoc/org/h2/tools/Console.html
 #
-H2JAR="${M2_REPO}/com/h2database/h2/2.2.220/h2-2.2.220.jar"
-PGJAR="${M2_REPO}/org/postgresql/postgresql/42.6.0/postgresql-42.6.0.jar"
+H2JAR="${M2_REPO}/com/h2database/h2/2.2.224/h2-2.2.224.jar"
+PGJAR="${M2_REPO}/org/postgresql/postgresql/42.7.1/postgresql-42.7.1.jar"
 if [ ! -r "${H2JAR}" ]; then
 	echo "ERROR: Please configure H2JAR location in this script."
 	exit 1
